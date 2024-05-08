35dddaee2ed3401435162d8bdef34882b92a2e68
omta <omtadayma2001@gmail.com>
2024-05-08 14:57:32 +0530

    add sticky navbar

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [sticky, setSticky] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  
+
   const navItems = (
     <>
       <li>
@@ -19,7 +35,9 @@ const Navbar = () => {
   );
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+      <div className={`max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 z-50 ${
+        sticky ? "sticky-navbar shadow-md bg-base-200 duration-300 transition-all ease-in-out" :""
+      }`}>
         <div className="navbar">
           <div className="navbar-start">
             <div className="dropdown">
