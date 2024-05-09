89adb8e2c43b85620ee7b7d7b84b031d0b6e2ddf
omta <omtadayma2001@gmail.com>
2024-05-09 16:38:11 +0530

    add theme

--- FRONTEND/src/components/Course.jsx
+++ FRONTEND/src/components/Course.jsx
@@ -8,8 +8,8 @@ const Course = () => {
   return (
     <>
       <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
-        <div className="mt-28 text-center">
-          <h1 className="text-2xl md:text-4xl">
+        <div className="mt-16 text-center">
+          <h1 className="pt-20 text-2xl md:text-4xl">
             We're delighted to have you{" "}
             <span className="text-pink-500">Here! :)</span>
           </h1>
