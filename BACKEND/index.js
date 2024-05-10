4e3fa1918e3ac9b523a2b74f857f94d793690d8a
omta <omtadayma2001@gmail.com>
2024-05-10 21:10:28 +0530

    mongodb connection

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,11 +1,15 @@
-const express = require("express")
+import express from 'express'
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
-})
+//connect to mongodb
+mongoose.connect(process.env.MONGODB)
+.then(()=>console.log('Connected to MongoDB'))
+.catch((error)=>console.error(error))
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
