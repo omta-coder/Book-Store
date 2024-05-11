090531bd62e7ec4d37be263827ff709667e5fb31
omta <omtadayma2001@gmail.com>
2024-05-11 10:21:24 +0530

    add addbook route

--- BACKEND/controllers/bookController.js
+++ BACKEND/controllers/bookController.js
@@ -1,11 +1,21 @@
 import Book from "../models/bookModel.js";
 
-export const getBook = async(req,res)=>{
+export const addBook = async(req,res)=>{
+    const {name,price,category,image,title} = req.body
     try {
-        const book = await Book.find();
-        res.status(200).json(book);
+        const book = await Book.create({name,price,category,image,title})
+        res.status(200).json({ message: "New book created successfully",book });
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
     }
 }
+export const getBook = async(req, res) => {
+    try {
+        const book = await Book.find();
+        res.status(200).json({ message: "get book successfully",book });
+    } catch (error) {
+        console.log("Error: ", error);
+        res.status(500).json(error);
+    }
+};
--- BACKEND/routes/bookRoute.js
+++ BACKEND/routes/bookRoute.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { getBook } from '../controllers/bookController.js';
+import { getBook,addBook } from '../controllers/bookController.js';
 
 const router = express.Router();
+
+router.post("/add",addBook)
 router.get('/',getBook);
 
 export default router;
