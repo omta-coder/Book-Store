26132290edff4cc7eb54800800ed207d23061685
omta <omtadayma2001@gmail.com>
2024-05-11 09:19:17 +0530

    create book route

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,15 +1,19 @@
 import express from 'express'
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import bookRoute from './routes/bookRoute.js'
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
+const app = express();
+app.use(express.json());
 
 //connect to mongodb
 mongoose.connect(process.env.MONGODB)
 .then(()=>console.log('Connected to MongoDB'))
 .catch((error)=>console.error(error))
+
+//defining routes
+app.use('/book', bookRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
