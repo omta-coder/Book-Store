e973b30744b3bb20a080a4b9387dde651cc398f3
omta <omtadayma2001@gmail.com>
2024-05-12 12:42:50 +0530

    hashed Password

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -1,12 +1,14 @@
 import User from "../models/userModel.js";
+import bcryptjs from 'bcryptjs'
 
 export const signup = async(req,res)=>{
     const {name,email,password} = req.body;
+    const hashedPassword = bcryptjs.hashSync("password",10)
     const user = await User.findOne({email})
     if(user){
         res.status(400).json({message:"User already exists"})
     }
-    const newUser = new User({name,email,password})
+    const newUser = new User({name,email,password:hashedPassword})
     try {
         await newUser.save();
         res.status(201).json({message:"User created successfully"})
