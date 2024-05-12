0829d0a5e00d5306f38818a24626eb080879dfa3
omta <omtadayma2001@gmail.com>
2024-05-12 13:14:13 +0530

    create login route

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -3,7 +3,7 @@ import bcryptjs from 'bcryptjs'
 
 export const signup = async(req,res)=>{
     const {name,email,password} = req.body;
-    const hashedPassword = bcryptjs.hashSync("password",10)
+    const hashedPassword =await bcryptjs.hashSync("password",10)
     const user = await User.findOne({email})
     if(user){
         res.status(400).json({message:"User already exists"})
@@ -11,9 +11,33 @@ export const signup = async(req,res)=>{
     const newUser = new User({name,email,password:hashedPassword})
     try {
         await newUser.save();
-        res.status(201).json({message:"User created successfully"})
+        res.status(201).json({message:"User created successfully",user:{
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        }})
     }
     catch(error){
         res.status(400).json({message:error.message})
     }
+}
+
+export const login = async(req,res)=>{
+    const {email,password} = req.body;
+    try {
+        const user = await User.findOne({email})
+        const validPassword = await bcryptjs.compareSync("password",user.password)
+        if(!user || !validPassword){
+            res.status(400).json({message:"Invalid User & password"})
+        }else{
+            res.status(200).json({message:"User logged in successfully",user:{
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            }})
+        }
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+
 }
