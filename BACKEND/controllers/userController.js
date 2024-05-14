2be0a66ee4451f9a84543d05721df9f780a0363b
omta <omtadayma2001@gmail.com>
2024-05-14 15:48:30 +0530

    Authentication functionality

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -3,18 +3,18 @@ import bcryptjs from 'bcryptjs'
 
 export const signup = async(req,res)=>{
     const {name,email,password} = req.body;
-    const hashedPassword =await bcryptjs.hashSync("password",10)
     const user = await User.findOne({email})
     if(user){
         res.status(400).json({message:"User already exists"})
     }
+    const hashedPassword =await bcryptjs.hashSync("password",10)
     const newUser = new User({name,email,password:hashedPassword})
     try {
         await newUser.save();
         res.status(201).json({message:"User created successfully",user:{
-            _id: user._id,
-            name: user.name,
-            email: user.email,
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
         }})
     }
     catch(error){
--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
 import Contacts from "./contacts/Contacts";
+import toast, { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/contact" element={<Contacts />} />
       </Routes>
+      <Toaster />
     </div>
     </>
   );
